0ace61de4cc060d7573b9996bf5752ac39fb060e
elodie-plt <elodiepolito@orange.fr>
2019-10-03 10:35:03 +0200

    paramétrage pour le login

---
+++ src/models/Login.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(100), unique=True, nullable=False)
+
+    def __init__(self, nom):
+        self.nom = nom
+
+    def __repr__(self):
+        return '<User %r>' % self.nom
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
